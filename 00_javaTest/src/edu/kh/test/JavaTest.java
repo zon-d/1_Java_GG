e31135c37d7e9827b58e79abc11fdd761447a1cf
zon-d <zon_d@icloud.com>
2022-08-05 15:13:10 +0900

    00_JavaTest 프로젝트 추가
    내가 한거 아님

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,27 @@
+package edu.kh.test;
+
+//한줄 주석: (컴파일러(번역기)가 해석하지 않는 부분)
+
+/*
+ *범위
+ *주석 
+ */
+
+// class : 자바 코드가 작성되는 영역
+
+public class JavaTest {
+	
+	//main 메서드 : 자바 애플리케이션(프로그램)을 실행하기 위해서 반드시 필요한 구문
+	public static void main(String[] args){
+		
+		int a = 10;
+		
+		//System.out.println("Hello! Hi! 안뇽! 하하하하하하");
+		System.out.println("println()은 한 줄 출력입니다.");
+		System.out.print("안녕하세요!\n");
+		System.out.println(100 + 200 + 300);
+		System.out.print(a+"\n");
+		System.out.printf("a는 %d이다.\n",a);
+	}
+	
+}

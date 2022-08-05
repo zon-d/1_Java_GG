d50c0afe91299bb5b3031fef6f9140d0e90c4f2d
zon-d <zon_d@icloud.com>
2022-08-05 17:12:15 +0900

    연습

--- 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
@@ -3,6 +3,15 @@ package edu.kh.variable.ex1;
 public class VariableExample6 {
 		public static void main(String[] args) {
 		
+			
+		String myDog = "철수";
+		String dogAge = "13 개월";
+		String dogGender = "수컷";
+		String breed = "비숑";
+		
+		System.out.println( "우리집 강아지 이름은 " + myDog + " 이고,나이는 " + dogAge + "이며, 성별은 " + dogGender + "이고, 견종은 " + breed + "이다." );
+		
+		
 		
 	}
 }

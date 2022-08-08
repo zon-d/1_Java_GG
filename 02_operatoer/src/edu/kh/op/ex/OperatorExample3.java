048c98beb6b258e0c756dcc7f29f4acf8a3a180e
zon-d <zon_d@icloud.com>
2022-08-08 11:05:02 +0900

    Create OperatorExample3.java

---
+++ 02_operatoer/src/edu/kh/op/ex/OperatorExample3.java
@@ -0,0 +1,39 @@
+package edu.kh.op.ex;
+
+public class OperatorExample3 {
+	 
+	
+	public static void main(String[] args) {
+	      
+	      // 비교 연산자 : == , !=, > , < , >= , <=
+	      
+	      // - 두 개의 피연산자의 크기를 비교하는 연산자
+	      // - 결과는 항상 논리값 (true/false)
+	      // - 복합 기호에서 '=' 기호가 항상 오른쪽
+	      
+	      
+	      
+	      int a = 10;
+	      int b = 20;
+	      
+	      
+	      System.out.println( a == b ); // false
+	      System.out.println( a != b ); // true
+	      System.out.println( a < b ); // true
+	      System.out.println( (a < b) != false ); // true
+	      
+	      System.out.println("-------------------------------------");
+	      
+	      
+	      System.out.println();
+	      
+	      
+	      
+	      
+	
+	
+	
+	
+	}
+
+}
